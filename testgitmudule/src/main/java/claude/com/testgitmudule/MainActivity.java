cce256cb8b229b27056db5fbb85b98c1a5bdd00d
ilnam327 <wunsong.xu@gmail.com>
2016-03-06 10:53:26 +0800

    new module

--- testgitmudule/src/main/java/claude/com/testgitmudule/MainActivity.java
+++ testgitmudule/src/main/java/claude/com/testgitmudule/MainActivity.java
@@ -1,11 +1,7 @@
 package claude.com.testgitmudule;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -15,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //999
 
     }
 
